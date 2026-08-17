e6afcd313b2e9a00650bccf9e96a676b243ed796
Martinvivas <129123814+Martinvivas@users.noreply.github.com>
2026-08-17 13:38:43 -0300

    5to test verde

--- calculadora.py
+++ calculadora.py
@@ -2,5 +2,6 @@ def sumar(numeros):
     if not numeros:
         return 0
     
-    partes = numeros.split(",")
+    numeros_limpios = numeros.replace("\n", ",")
+    partes = numeros_limpios.split(",")
     return sum(int(n) for n in partes)
