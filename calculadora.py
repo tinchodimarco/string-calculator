a7971ba2533e720ec49a18f11363b583786e42bb
Martinvivas <129123814+Martinvivas@users.noreply.github.com>
2026-08-17 13:40:50 -0300

    6to test verde

--- calculadora.py
+++ calculadora.py
@@ -1,7 +1,14 @@
 def sumar(numeros):
     if not numeros:
         return 0
+
+    delimitador = ","
     
-    numeros_limpios = numeros.replace("\n", ",")
+    if numeros.startswith("//"):
+        encabezado, numeros = numeros.split("\n", 1)
+        delimitador = encabezado[2:]  
+
+    numeros_limpios = numeros.replace("\n", ",").replace(delimitador, ",")
     partes = numeros_limpios.split(",")
+    
     return sum(int(n) for n in partes)
